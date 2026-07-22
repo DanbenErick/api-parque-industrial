a1dde933e53a2a3be894b40c369903910e9b860f
Danben Erick <danbencb08@outlook.es>
2026-07-21 22:37:50 -0500

    novedades

--- src/repositories/periodoRepository.ts
+++ src/repositories/periodoRepository.ts
@@ -25,23 +25,23 @@ export class PeriodoRepository {
           };
         };
     public create = async (periodo: any): Promise<number> => {
-          const { mes_anio, factor_multiplicador, tarifa_kwh, tarifa_kwh_tr, tarifa_kwh_punta, costo_potencia, tarifa_mantenimiento_normal, tarifa_mantenimiento_tiempo_real, fecha_inicio, fecha_fin, fecha_emision_recibo, fecha_vencimiento, fecha_corte, fecha_inicio_lectura, fecha_fin_lectura, created_by } = periodo;
+          const { mes_anio, factor_multiplicador, tarifa_kwh, tarifa_kwh_tr, tarifa_kwh_punta, costo_potencia, costo_potencia_fuera_punta, tarifa_mantenimiento_normal, tarifa_mantenimiento_tiempo_real, fecha_inicio, fecha_fin, fecha_emision_recibo, fecha_vencimiento, fecha_corte, fecha_inicio_lectura, fecha_fin_lectura, created_by } = periodo;
           const [result]: any = await this.db.query(
             `INSERT INTO periodo_facturacion (
-      mes_anio, factor_multiplicador, tarifa_kwh, tarifa_kwh_tr, tarifa_kwh_punta, costo_potencia, tarifa_mantenimiento_normal, tarifa_mantenimiento_tiempo_real, fecha_inicio, fecha_fin, fecha_emision_recibo, fecha_vencimiento, fecha_corte, fecha_inicio_lectura, fecha_fin_lectura, created_by
-    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-            [mes_anio, factor_multiplicador || 1.0000, tarifa_kwh, tarifa_kwh_tr || 0, tarifa_kwh_punta || 0, costo_potencia || 0, tarifa_mantenimiento_normal, tarifa_mantenimiento_tiempo_real, fecha_inicio, fecha_fin, fecha_emision_recibo, fecha_vencimiento, fecha_corte, fecha_inicio_lectura, fecha_fin_lectura, created_by]
+      mes_anio, factor_multiplicador, tarifa_kwh, tarifa_kwh_tr, tarifa_kwh_punta, costo_potencia, costo_potencia_fuera_punta, tarifa_mantenimiento_normal, tarifa_mantenimiento_tiempo_real, fecha_inicio, fecha_fin, fecha_emision_recibo, fecha_vencimiento, fecha_corte, fecha_inicio_lectura, fecha_fin_lectura, created_by
+    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+            [mes_anio, factor_multiplicador || 1.0000, tarifa_kwh, tarifa_kwh_tr || 0, tarifa_kwh_punta || 0, costo_potencia || 0, costo_potencia_fuera_punta || 0, tarifa_mantenimiento_normal, tarifa_mantenimiento_tiempo_real, fecha_inicio, fecha_fin, fecha_emision_recibo, fecha_vencimiento, fecha_corte, fecha_inicio_lectura, fecha_fin_lectura, created_by]
           );
           return result.insertId;
         };
     public update = async (id: number, periodo: any): Promise<number> => {
-          const { mes_anio, factor_multiplicador, tarifa_kwh, tarifa_kwh_tr, tarifa_kwh_punta, costo_potencia, tarifa_mantenimiento_normal, tarifa_mantenimiento_tiempo_real, fecha_inicio, fecha_fin, fecha_emision_recibo, fecha_vencimiento, fecha_corte, fecha_inicio_lectura, fecha_fin_lectura } = periodo;
+          const { mes_anio, factor_multiplicador, tarifa_kwh, tarifa_kwh_tr, tarifa_kwh_punta, costo_potencia, costo_potencia_fuera_punta, tarifa_mantenimiento_normal, tarifa_mantenimiento_tiempo_real, fecha_inicio, fecha_fin, fecha_emision_recibo, fecha_vencimiento, fecha_corte, fecha_inicio_lectura, fecha_fin_lectura } = periodo;
           const [result]: any = await this.db.query(
             `UPDATE periodo_facturacion 
-     SET mes_anio = ?, factor_multiplicador = ?, tarifa_kwh = ?, tarifa_kwh_tr = ?, tarifa_kwh_punta = ?, costo_potencia = ?,
+     SET mes_anio = ?, factor_multiplicador = ?, tarifa_kwh = ?, tarifa_kwh_tr = ?, tarifa_kwh_punta = ?, costo_potencia = ?, costo_potencia_fuera_punta = ?,
          tarifa_mantenimiento_normal = ?, tarifa_mantenimiento_tiempo_real = ?, fecha_inicio = ?, fecha_fin = ?, fecha_emision_recibo = ?, fecha_vencimiento = ?, fecha_corte = ?, fecha_inicio_lectura = ?, fecha_fin_lectura = ?
      WHERE id = ? AND deleted_at IS NULL`,
-            [mes_anio, factor_multiplicador, tarifa_kwh, tarifa_kwh_tr || 0, tarifa_kwh_punta || 0, costo_potencia || 0, tarifa_mantenimiento_normal, tarifa_mantenimiento_tiempo_real, fecha_inicio, fecha_fin, fecha_emision_recibo, fecha_vencimiento, fecha_corte, fecha_inicio_lectura, fecha_fin_lectura, id]
+            [mes_anio, factor_multiplicador, tarifa_kwh, tarifa_kwh_tr || 0, tarifa_kwh_punta || 0, costo_potencia || 0, costo_potencia_fuera_punta || 0, tarifa_mantenimiento_normal, tarifa_mantenimiento_tiempo_real, fecha_inicio, fecha_fin, fecha_emision_recibo, fecha_vencimiento, fecha_corte, fecha_inicio_lectura, fecha_fin_lectura, id]
           );
           return result.affectedRows;
         };
